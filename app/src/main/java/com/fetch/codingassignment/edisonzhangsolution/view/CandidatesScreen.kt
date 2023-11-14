9aeb408d6063b405358c78b7e368d372ac2ef409
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2023-11-14 13:23:35 -0500

    update the UI in preparation of new funcs

--- app/src/main/java/com/fetch/codingassignment/edisonzhangsolution/view/CandidatesScreen.kt
+++ app/src/main/java/com/fetch/codingassignment/edisonzhangsolution/view/CandidatesScreen.kt
@@ -1,42 +1,106 @@
 package com.fetch.codingassignment.edisonzhangsolution.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.fetch.codingassignment.edisonzhangsolution.R
 import com.fetch.codingassignment.edisonzhangsolution.model.Candidate
 import com.fetch.codingassignment.edisonzhangsolution.viewmodel.MainViewModel
 
 @Composable
 fun CandidatesScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
     val candidatesListState by viewModel.candidatesListState
-    Box(modifier = Modifier.fillMaxSize()) {
-        when{
-            candidatesListState.loading -> {
-                CircularProgressIndicator(modifier.align(Alignment.Center))
-            }
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppHeader()
+        Box(modifier = Modifier.fillMaxSize()) {
+            when{
+                candidatesListState.loading -> {
+                    CircularProgressIndicator(modifier.align(Alignment.Center))
+                }
 
-            candidatesListState.error!=null -> {
-                Text(candidatesListState.error ?: "ERROR OCCURED")
+                candidatesListState.error!=null -> {
+                    Text(candidatesListState.error ?: "ERROR OCCURED")
+                }
+                else -> {
+                    // Display Candidates
+                    CandidateTable(candidatesListState.list)
+                }
             }
-            else -> {
-                // Display Candidates
-                CandidateTable(candidatesListState.list)
-            }
+        }
+    }
+
+}
+
+@Composable
+fun AppHeader(){
+    Row(modifier = Modifier
+        .fillMaxHeight(0.15f)
+        .fillMaxWidth()
+        .padding(2.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween){
+        Image(painter = painterResource(id = R.drawable.fetch_rewards_logo), contentDescription = "fetch app logo")
+        ButtonGroup()
+    }
+}
+
+@Composable
+fun ButtonGroup() {
+    Row(modifier = Modifier
+        .fillMaxHeight()
+        .padding(2.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "sync")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        DropDownFilter()
+    }
+}
+
+@Composable
+fun DropDownFilter() {
+    Box {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "filter")
+        }
+        DropdownMenu(
+            expanded = false,
+            onDismissRequest = {  }
+        ) {
+            DropdownMenuItem(
+                text = {Text("1")},
+                onClick = {}
+            )
+            DropdownMenuItem(
+                text = {Text("2")},
+                onClick = {}
+            )
         }
     }
 }
