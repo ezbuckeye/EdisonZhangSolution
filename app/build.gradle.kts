2cdeb9c64cb11f7fb48edab2f11378558098b9a4
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2023-11-14 14:09:27 -0500

    another necessary room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     val room_version = "2.6.0"
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
