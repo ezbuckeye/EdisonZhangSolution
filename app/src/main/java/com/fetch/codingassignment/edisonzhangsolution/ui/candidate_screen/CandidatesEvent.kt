2a52e8ecf62eedaf0690fe8756088ceb9abca2b7
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2023-11-14 14:54:48 -0500

    defined possible CandidatesEvent

---
+++ app/src/main/java/com/fetch/codingassignment/edisonzhangsolution/ui/candidate_screen/CandidatesEvent.kt
@@ -0,0 +1,9 @@
+package com.fetch.codingassignment.edisonzhangsolution.ui.candidate_screen
+
+sealed class CandidatesEvent {
+    object OnSyncClick: CandidatesEvent()
+    object OnDropdownClick: CandidatesEvent()
+    object OnDropdownDismiss: CandidatesEvent()
+    data class OnListIdSelect(val listId: Integer): CandidatesEvent()
+}
+
